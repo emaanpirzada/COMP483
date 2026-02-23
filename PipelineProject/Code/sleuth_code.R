f6880aca6ee96485bf64c6c404bf80ceb5f4308c
Emaan Pirzada <emaanpirzada@gmail.com>
2026-02-23 12:12:58 -0600

    Added Python and R codes

---
+++ PipelineProject/Code/sleuth_code.R
@@ -0,0 +1,36 @@
+library(sleuth)
+library(dplyr)
+
+# Gets all the kallisto samples
+samples <-dir("..Outputs/Quantified")
+samples
+
+# Assigns the conditions
+condition <- c("2dpi", "6dpi", "2dpi", "6dpi")
+
+# Creates a metadata table
+metadata_table <- data.frame(sample = samples, condition = condition, stringsAsFactors=FALSE)
+
+# Adds a path column to the metadata table
+metadata_table <- metadata_table %>%
+   mutate(path=file.path("../Outputs/Quantified", sample))
+
+# Creates the sleuth object
+sleuth_object <- sleuth_prep(metadata_table, ~ condition)
+
+# Fits the model
+sleuth_object <- sleuth_fit(sleuth_object, ~ condition, 'full')
+sleuth_object <- sleuth_fit(sleuth_object, ~1, 'reduced')
+
+# Runs a likelihood ratio test
+sleuth_object <- sleuth_lrt(sleuth_object, 'reduced', 'full')
+
+# Extracts results
+results_table <- sleuth_results(sleuth_object, 'reduced:full', 'lrt')
+
+# Filters only the significant transcrips (FDR < 0.05)
+sig_transcripts <- results_table %>%
+   filter(qval < 0.05) %>%
+   select(target_id, test_stat, pval, qval)
+
+write.table(sig, file = "../Outputs/sleuth_report.txt", sep = "\t", quote = FALSE, row.names = FALSE, append = TRUE)
